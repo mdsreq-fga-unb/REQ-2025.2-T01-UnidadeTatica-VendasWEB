36cac4e40b98278d30c14740837eff30311ebb0d
Pedro <pedrovieira1355@gmail.com>
2025-11-29 22:42:16 -0300

    feat: redirecionamento ao sac via whatsappApi(mudar o placeholder para o celular do admin posteriormente)

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,7 @@
-
-
 import express from 'express';
 import clienteRoutes from './routes/clienteRoutes.js';
 import cors from 'cors';
+import sacRoutes from "./routes/sacRoutes.js";
 
 const app = express();
 
@@ -11,6 +10,7 @@ app.use(express.json());
 
 
 app.use('/api', clienteRoutes);
+app.use('/api', sacRoutes);
 
 
 
